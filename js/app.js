4f57b372a835bf58b57dc6bd5caa6585edda9f18
hsjung22 <hsjung22@gmail.com>
2017-07-23 11:48:23 -0700

    feat: Move model logic into Cat function, outside of the ViewModel

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var ViewModel = function() {
+var Cat = function() {
   this.clickCount = ko.observable(0);
   this.name = ko.observable('Tabby');
   this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
@@ -18,9 +18,13 @@ var ViewModel = function() {
       return "Teen"
     }
   }, this);
+}
+
+var ViewModel = function() {
+  this.currentCat = ko.observable( new Cat() );
 
   this.incrementCounter = function() {
-    this.clickCount(this.clickCount() + 1);
+    this.currentCat().clickCount(this.currentCat().clickCount() + 1);
   };
 }
 
