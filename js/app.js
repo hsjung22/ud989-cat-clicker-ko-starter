93da965d30bfc93f6c25f410a763864f5f1e7389
hsjung22 <hsjung22@gmail.com>
2017-07-23 11:16:45 -0700

    feat: Add nicknames display feature using foreach

--- js/app.js
+++ js/app.js
@@ -3,6 +3,11 @@ var ViewModel = function() {
   this.name = ko.observable('Tabby');
   this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
   this.imgAttribution = ko.observable('https://www.flickr.com/photos/big')
+  this.nicknames = ko.observableArray([
+    { nickname: "TabTab" },
+    { nickname: "Mr. T" },
+    { nickname: "T-Bone" }
+  ])
 
   this.level = ko.computed(function() {
     if(this.clickCount() < 10) {
